168fd9bdbfe84432364241a880a28bfb3b1fe868
mike <michael@hotmail.fr>
2020-10-04 19:01:27 +0200

    first version of monopoly functions

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+
+  <head>
+
+      <script src = 'script.js'></script>
+
+  </head>
+
+
+
+    <h1>Monopoly</h1>
+
+    <h2>Tour3</h2>
+
+
+    <h3>Tom : 1500 dollars, 1 maison orange, 1 maison violette</h3>
+
+    <h3>Eva : 1500 dollars, 1 maison orange, 1 maison violette</h3>
+
+
+    <h3>Joueur1 : 1500 dollars, 1 maison bleu, 1 maison rouge</h3>
+
+    <h3>Joueur2 : 1500 dollars, 1 maison jaune, 1 maison marron</h3>
+
+
+      
+    <h1> C'est au tour de Tom d'agir (action annexe)</h1>
+
+
+
+     <button>Lancer la réflexion</button>
+
+
+
+
+</html>
--- script.js
+++ script.js
@@ -1,134 +1,238 @@
-var player1 = "Mathéo";
-var player2 = "Ludovic";
-var b79 = "B79";
-var board = [0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+var brownProperty1 = 1;
+
+var brownProperty2 = 2;
+
+var lightBlueProperty1 = 3;
+
+var lightBlueProperty2 = 4;
+
+var purpleProperty = 5;
+
+var redProperty = 6;
+
+var greenProperty = 7;
+
+var yellowProperty = 8;
 
 
 
-var players = [player1, player2, b79];
+var ai1 = { properties: {brown: [brownProperty1], lightBlue: [lightBlueProperty2] }, position: 0,  purse: 1500}
+
+var ai2 = { properties: {brown: [brownProperty2], lightBlue: [lightBlueProperty1]}, position: 0, purse: 1500}
+
+var player1 = { properties: {purple: [purpleProperty], red: [redProperty]}, position: 0, purse: 1500}
+
+var player2 = { properties: {green: [greenProperty], yellow: [yellowProperty]}, position: 0, purse: 1500}
+
+var monopoly;
 
 
 
-//first player to play
-var playerTurn = 0;
-
-var diceElement = $("#diceElement").html();
-
-var playerTurnIntro = "C'est le tour de ";
-
-var player1SquareIntro = $('#player1Square').html();
-var player2SquareIntro = $('#player2Square').html();
-var b79SquareIntro = $('#b79Square').html();
-
-
-var player1Square = board[0];
-var player2Square = board[0];
-var b79Square = board[0];
-
-
-
-
-
-$('#playerTurnText').html(playerTurnIntro + 'player1');
-
-
-
-
-$('#player1Square').html(player1SquareIntro + player1Square);
-$('#player2Square').html(player2SquareIntro + player2Square);
-$('#b79Square').html(b79SquareIntro + b79Square);
+var diceTwo = {num: 2, probability: 1};
+var diceThree = {num: 3, probability: 2};
+var diceFour = {num: 4, probability: 3};
+var diceFive = {num: 5, probability: 4};
+var diceSix = {num: 6, probability: 5};
+var diceSeven = {num: 7, probability: 6};
+var diceEight = {num: 8, probability: 5};
+var diceNine = {num: 9, probability: 4};
+var diceTen = {num: 10, probability: 3};
+var diceEleven = {num: 11, probability: 2};
+var diceTwelve =  {num: 12, probability: 1};
 
 
 
 
+var dicesResults = [diceTwo, diceThree, diceFour, diceFive, diceSix, diceSeven, diceSeven, diceEight, diceNine, diceTen, diceEleven, diceTwelve];
+
+
+var players = [ai1, ai2, player1, player2, monopoly];
+
+
+var gameBoard = { playerInPlay: ai1 , state: 'none', activeDiceLauncher: ai1, playerInPlay: ai1};
+
+
+console.log(player2.purse);
 
 
 
-function launchDice(){
+function alphabeta(depth, playerNum){
 
-    var diceResult = Math.floor((Math.random()) * 11) ;
+    if(players[playerNum] == a1 || players[playerNum] == ai2 ){
+         
+        generateAiMoves();
 
-    $("#diceElement").html(diceElement + diceResult);
-    $('#playerTurn').html();
+        var bestScore = [];
+
+        for(i=0; i < aiMoveList; i++){
+
+            makeMove(aiMoveList[i].move);
+
+            score = alphabeta(depth-1, gameBoard.playerInPlay);
+
+            if(score[playerNum] > bestScore[playerNum]){
+
+                bestScore = score
+            }
+
+            takeMove(aiMoveList[i]);
+
+          }
+          
+
+       } else if (players[playerNum] == player1 || players[playerNum] == player2 ){
+
+              
+        generateHumanPlayerMoves();
+
+        var bestScore = [];
+
+            makeMove(bestHumanMove.move);
+
+            score = alphabeta(depth-1, gameBoard.playerInPlay);
+
+            if(score[playerNum] > bestScore[playerNum]){
+
+                bestScore = score
+            }
+
+            takeMove(bestHumanMove.move);
+
+          
+
+       } else if (players[playerNum] == monopoly){
+
+              aiEvaluation(); //get the array at the current step, to compare its values with the result obtained, to make the ratio gain-loss/probability
+          
+              var choosedScore = [];
+
+    
+            for (i = 0; i < dicesResults.length; i++){
+
+             layDice(dicesResults[i]); //equivalent of the makeMove function, except we check the result of a dice launching
+
+             //we simulate the consequences of a launch of each dice combination
+
+
+              score = diceScore(dicesResults[i].probability,thinkingAi, evaluationArray, alphabeta(depth-1, gameBoard.playerInPlay));
+              
+
+              if(score > choosedScore){
+
+                 choosedScore = score;
+              }
+              
+              takeDiceBack(); //equivalent of takeMove
+           }
+ 
+      }
+
+}
 
 
 
-    if(playerTurn == 0){
-        player1Square += diceResult;
 
-        if(player1Square >= 40){
-            player1Square-=40;
 
-        }
-    }else if (playerTurn == 1){
-        player2Square += diceResult;
-        if(player2Square >= 40){
-            player2Square -= 40;
-        }
+function generateAiMoves(){
+
+    
+}
+
+
+
+function generateHumanPlayerMoves(){
+
+
+}
+
+
+function diceScore(probability,thinkingAi, evaluationArray, newEvalArray){
+    
+     //launch dice : 7 (meaning, probability 11)
+    //ex: [400, 150, 200, 150];
+   //if ai1 thinks, the score will be 
+    return probability * [evaluationArray[currentTurn+1][thinkingAi] - newEvalArray[thinkingAi]];
+
+}
+
+
+function generateDicesLaunches(){
+
+
+}
+
+
+
+function aiEvaluation(){
+    
+    var evaluationArray;
+
+    return evaluationArray;
+
+}
+
+
+
+
+function humanPlayerEvaluation(){
+    
+   var humanEvaluationArray;
+    
+    return humanEvaluationArray;
+
+}
+
+
+
+function layDice(diceResult){
+   
+    //generate a move on the board. Ex : player1 goes from departure to imposition square
+    
+    movePlayer(gameBoard.playerInPlay, diceResult);
+
+}
+
+
+function takeDiceBack(){
+
+
+}
+
+
+function movePlayer(player, diceResult){
+   
+    
+    if(player.position + diceResult > 39){
+
+        player.position = ((player.position + diceResult) - 39);
 
     } else {
 
-        if(b79Square >= 40){
-            b79Square -= 40;
-        }
-
-        b79Square+=diceResult;
-
+        player.position += diceResult;
     }
-    
-    
-
-
-    if( playerTurn == 2){
-        playerTurn = 0;
-
-
-    }else{
-        playerTurn+=1;
-    }
-
-    for(index= 0; index< 3; index++){
-        if(playerTurn == index){
-
-            $('#playerTurnText').html(playerTurnIntro + players[playerTurn]);
-
-        }
-    }
-
-
-
-
-
-    $('#moveDescription').html(players[playerTurn-1]+ " a avancé de " + diceResult  +" cases");
-
-
-
-
-    $('#player1Square').html(player1SquareIntro + player1Square);
-    $('#player2Square').html(player2SquareIntro + player2Square);
-    $('#b79Square').html(b79SquareIntro + b79Square);
-
-
-
-
-
-
-
-
-
-
-//end of launcDice function
 
 }
 
 
 
-function searchMove(){
+function nextPlayerToLaunchDice(){
 
-       
+     if(gameBoard.activeDiceLauncher == 3){
+
+           gameBoard.activeDiceLauncher = 0
+     } else {
+
+        gameBoard.activeDiceLauncher += 1
+     }
 }
 
 
 
+function makeMove(move){
 
+    
 
+    
+    nextPlayerNum = 0
+
+}
