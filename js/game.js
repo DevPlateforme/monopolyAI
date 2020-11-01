cc3a088878ae46011c1fff54bf919a8141aa67f2
mike <michael@hotmail.fr>
2020-11-01 17:02:02 +0100

    creation of the makeMove function

--- js/game.js
+++ js/game.js
@@ -13,37 +13,13 @@ function newGame(){
 
         initPawnsPositions();
 
-    //ai1 launches first , then ai2 does it.
-       
-       for(i=0; i < 2 ; i++){
-
-        
-        launchDice();
-
-            console.log('résultat du lancer de dés: ' + players[nextDiceLauncherIndex -1].name + ' a obtenu un '  + diceResult);
-
-        movePiece(diceResult);
-       
-       }
-      
-       //Then, player1 can play
-        
-       displayDiceLaunchButton();
-
-
-       //AFTER player 2
-
-
+        launchGameCycle();
 
 
 
 }
 
 
-
-
-
-//diceLaunch
 
 
 
@@ -117,7 +93,7 @@ function movePiece(){
 
      } else if (squaresArray[updatedPosition].owner != lastDiceLauncher) {
 
-        
+
         
         let payer = gameBoard.activePlayer;
 
@@ -143,3 +119,223 @@ function movePiece(){
 
    }
 
+
+
+
+   function launchGameCycle(){
+
+
+    
+        //GAME CYCLE
+
+
+        //THE NEXT DICE LAUNCHER LAUNCHES THE DICE 
+
+        launchDice();
+
+        //IT TRIGGERS THE PROPOSITION 1 , TRIGGERING AN ANSWER, TRIGGERING THE PROPOSITION 2 , TRIGGERING ANOTHER ANSWER...UP UNTIL THE 4th PROPOSITION
+        
+
+   }
+
+
+
+
+   function makeMove(){
+
+
+
+       //PROPOSITIONS
+
+       if( gameBoard.state == proposition1 || gameBoard.state == proposition2  || gameBoard.state == proposition3  || gameBoard.state == proposition4){
+            
+      
+        let proposition = gameBoard.state;
+
+        
+          if(gameBoard.state == proposition1){
+
+ 
+             gameBoard.state = answer1;
+
+             answer1.propositionSubmitted = proposition;
+
+
+
+          } else if(gameBoard.state == proposition2){
+
+            gameBoard.state = answer2;
+
+            answer2.propositionSubmitted = proposition;
+
+    
+
+         } else if(gameBoard.state == proposition3){
+
+           gameBoard.state = answer3;
+
+           answer3.propositionSubmitted = proposition;
+
+     
+         } else if(gameBoard.state == proposition4){
+
+          gameBoard.state = answer4;
+
+          answer4.propositionSubmitted = proposition;
+
+
+        }
+
+
+        
+
+
+       }
+       
+
+
+      else if( gameBoard.state == answer1 || gameBoard.state == answer2|| gameBoard.state == answer3|| gameBoard.state == answer4){
+
+              
+          if(gameBoard.state == anwer1){
+
+             gameBoard.state = proposition2;
+
+          } else if(gameBoard.state == anwer2){
+
+             gameBoard.state = proposition3;
+
+
+         } else if(gameBoard.state == anwer3){
+
+           gameBoard.state = proposition4;
+ 
+         } else if(gameBoard.state == anwer4){
+
+          gameBoard.state = mortgageDecision1;
+
+        }
+
+
+
+        let answer = gameBoard.state;
+
+        makeAnswer(answer);
+        
+      }
+
+    //ANSWER 
+
+    //MORTGAGE DECISIONS
+
+
+    else if (gameBoard.state == mortgageDecision1 || gameBoard.state == mortgageDecision2 || gameBoard.state == mortgageDecision3 || gameBoard.state == mortgageDecision4   ){
+
+     
+    if (gameBoard.state == mortgageDecision1){
+
+        gameBoard.state = mortgageDecision2;
+
+    } else if (gameBoard.state == mortgageDecision2){
+
+        gameBoard.state = mortgageDecision3;
+
+    } else if (gameBoard.state == mortgageDecision3){
+
+        gameBoard.state = mortgageDecision4;
+
+    } else if (gameBoard.state ==  mortgageDecision4){
+
+        gameBoard.state = houseSellingDecision1;
+    } 
+
+
+
+  }
+
+
+  else if (gameBoard.state == houseSellingDecision1 || gameBoard.state == houseSellingDecision2 || gameBoard.state == houseSellingDecision3 || gameBoard.state == houseSellingDecision4   ){
+
+
+    //HOUSE SELLING DECISIONS
+
+    if (gameBoard.state == houseSellingDecision1){
+
+        gameBoard.state = houseSellingDecision2;
+
+    } else if (gameBoard.state == houseSellingDecision2){
+
+        gameBoard.state = houseSellingDecision3;
+
+    } else if (gameBoard.state == houseSellingDecision3){
+
+        gameBoard.state = houseSellingDecision4;
+
+    } else if (gameBoard.state ==  houseSellingDecision4){
+
+
+        gameBoard.state = houseSellingDecision1;
+
+
+
+    } 
+
+
+ }
+
+                //HOUSE BUYING DECISIONS
+
+
+
+    else if (gameBoard.state == houseBuyingDecision1 || gameBoard.state == houseBuyingDecision2 || gameBoard.state == houseBuyingDecision3 || gameBoard.state == houseBuyingDecision4 ){
+             
+
+    if (gameBoard.state == houseBuyingDecision1){
+
+        gameBoard.state = houseBuyingDecision2;
+
+    } else if (gameBoard.state == houseBuyingDecision2){
+
+        gameBoard.state = houseBuyingDecision3;
+
+    } else if (gameBoard.state == houseBuyingDecision3){
+
+        gameBoard.state = houseBuyingDecision4;
+
+    } else if (gameBoard.state == houseBuyingDecision4){
+
+        gameBoard.state = launchDices;
+
+    } 
+
+
+  }
+
+
+
+}
+
+
+
+
+
+function makeAnswer(answer){
+
+
+    if(answer.verdict = no){
+
+        //nothing happens
+
+        return;
+
+
+    } else {
+
+        //accept trade
+
+
+    }
+
+
+
+ }
